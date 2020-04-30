e68bae6eae9e6d63b35b817caf0166e2a91d825a
pranithkumar <praneeth970@gmail.com>
2020-04-30 04:21:27 +0000

    odata table button toggle working in controller

---
+++ webapp/controller/S1.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("FioriTraining.FioriTraining.controller.S1", {
+		onInit: function () {
+			var oTable = this.getView().byId("tab1");
+			oTable.setVisible(false);
+		},
+		
+		onGetData: function() {
+			var oTable = this.getView().byId("tab1");
+			var obj = this.getOwnerComponent().getModel();
+			var sPath = "/emps";
+			obj.read(sPath,{
+				success: function(oDataResult){
+					// 1.create the json model(class)
+					// 2.set data on model
+					// 3.set model on the control
+					
+					//1
+					var oModelJson = new sap.ui.model.json.JSONModel();
+					//2
+					oModelJson.setData(oDataResult.results);
+					//3
+					oTable.setModel(oModelJson,"jsonMod");
+					
+					sap.m.MessageToast.show("Success");
+				}
+			});
+			// var oTable = this.getView().byId("tab1");
+			oTable.setVisible(true);
+		}
+	});
+});
